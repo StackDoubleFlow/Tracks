531cd0270534a45dc243952a56bc62818f33840f
FernTheDev <15272073+Fernthedev@users.noreply.github.com>
2023-11-01 14:08:45 -0400

    Use CJD JSON header

--- shared/Json.h
+++ shared/Json.h
@@ -1,143 +1,146 @@
 #pragma once
+#include "custom-json-data/shared/JsonUtils.h"
 
-#include <optional>
-#include "beatsaber-hook/shared/config/rapidjson-utils.hpp"
-#include "Vector.h"
-#include "TLogger.h"
+// #pragma once
 
-namespace NEJSON {
+// #include <optional>
+// #include "beatsaber-hook/shared/config/rapidjson-utils.hpp"
+// #include "Vector.h"
+// #include "TLogger.h"
 
-static std::optional<bool> ReadOptionalBool(rapidjson::Value const& object, std::string_view const key) {
-  auto itr = object.FindMember(key.data());
-  if (itr != object.MemberEnd()) {
-    if (itr->value.IsString()) {
-      std::string boolS = itr->value.GetString();
+// namespace NEJSON {
 
-      if (boolS == "true") {
-        return true;
-      }
+// static std::optional<bool> ReadOptionalBool(rapidjson::Value const& object, std::string_view const key) {
+//   auto itr = object.FindMember(key.data());
+//   if (itr != object.MemberEnd()) {
+//     if (itr->value.IsString()) {
+//       std::string boolS = itr->value.GetString();
 
-      TLogger::GetLogger().error("ReadOptionalBool: THE VALUE IS A STRING WHY! value: \"%s\"", boolS.c_str());
-      return false;
-    }
+//       if (boolS == "true") {
+//         return true;
+//       }
 
-    return itr->value.GetBool();
-  }
-  return std::nullopt;
-}
+//       TLogger::GetLogger().error("ReadOptionalBool: THE VALUE IS A STRING WHY! value: \"%s\"", boolS.c_str());
+//       return false;
+//     }
 
-static std::optional<std::string_view> ReadOptionalString(rapidjson::Value const& object, std::string_view const key) {
-  auto itr = object.FindMember(key.data());
-  if (itr != object.MemberEnd() && itr->value.IsString()) {
-    return itr->value.GetString();
-  }
-  return std::nullopt;
-}
+//     return itr->value.GetBool();
+//   }
+//   return std::nullopt;
+// }
 
-static std::optional<float> ReadOptionalFloat(rapidjson::Value const& object, std::string_view const key) {
-  auto itr = object.FindMember(key.data());
-  if (itr != object.MemberEnd()) {
-    return itr->value.GetFloat();
-  }
-  return std::nullopt;
-}
+// static std::optional<std::string_view> ReadOptionalString(rapidjson::Value const& object, std::string_view const key) {
+//   auto itr = object.FindMember(key.data());
+//   if (itr != object.MemberEnd() && itr->value.IsString()) {
+//     return itr->value.GetString();
+//   }
+//   return std::nullopt;
+// }
 
-static std::optional<int> ReadOptionalInt(rapidjson::Value const& object, std::string_view const key) {
-  auto itr = object.FindMember(key.data());
-  if (itr != object.MemberEnd()) {
-    return itr->value.GetInt();
-  }
-  return std::nullopt;
-}
+// static std::optional<float> ReadOptionalFloat(rapidjson::Value const& object, std::string_view const key) {
+//   auto itr = object.FindMember(key.data());
+//   if (itr != object.MemberEnd()) {
+//     return itr->value.GetFloat();
+//   }
+//   return std::nullopt;
+// }
 
-static std::optional<NEVector::Vector2> ReadOptionalVector2(rapidjson::Value const& object,
-                                                            std::string_view const key) {
-  auto itr = object.FindMember(key.data());
-  if (itr != object.MemberEnd() && itr->value.Size() >= 2) {
-    float x = itr->value[0].GetFloat();
-    float y = itr->value[1].GetFloat();
-    return NEVector::Vector2(x, y);
-  }
-  return std::nullopt;
-}
+// static std::optional<int> ReadOptionalInt(rapidjson::Value const& object, std::string_view const key) {
+//   auto itr = object.FindMember(key.data());
+//   if (itr != object.MemberEnd()) {
+//     return itr->value.GetInt();
+//   }
+//   return std::nullopt;
+// }
 
-// Used for note flip
-static std::optional<NEVector::Vector2> ReadOptionalVector2_emptyY(rapidjson::Value const& object,
-                                                                   std::string_view const key) {
-  auto itr = object.FindMember(key.data());
+// static std::optional<NEVector::Vector2> ReadOptionalVector2(rapidjson::Value const& object,
+//                                                             std::string_view const key) {
+//   auto itr = object.FindMember(key.data());
+//   if (itr != object.MemberEnd() && itr->value.Size() >= 2) {
+//     float x = itr->value[0].GetFloat();
+//     float y = itr->value[1].GetFloat();
+//     return NEVector::Vector2(x, y);
+//   }
+//   return std::nullopt;
+// }
 
-  if (itr != object.MemberEnd() && itr->value.Size() >= 1) {
-    float x = itr->value[0].GetFloat();
-    float y = 0;
+// // Used for note flip
+// static std::optional<NEVector::Vector2> ReadOptionalVector2_emptyY(rapidjson::Value const& object,
+//                                                                    std::string_view const key) {
+//   auto itr = object.FindMember(key.data());
 
-    if (itr->value.Size() > 1) {
-      y = itr->value[1].GetFloat();
-    }
-    return NEVector::Vector2(x, y);
-  }
-  return std::nullopt;
-}
+//   if (itr != object.MemberEnd() && itr->value.Size() >= 1) {
+//     float x = itr->value[0].GetFloat();
+//     float y = 0;
 
-using OptPair = std::pair<std::optional<float>, std::optional<float>>;
+//     if (itr->value.Size() > 1) {
+//       y = itr->value[1].GetFloat();
+//     }
+//     return NEVector::Vector2(x, y);
+//   }
+//   return std::nullopt;
+// }
 
-static OptPair ReadOptionalPair(rapidjson::Value const& object, std::string_view const key) {
-  auto itr = object.FindMember(key.data());
+// using OptPair = std::pair<std::optional<float>, std::optional<float>>;
 
-  if (itr != object.MemberEnd() && itr->value.Size() >= 1) {
-    float x = itr->value[0].GetFloat();
-    float y = 0;
+// static OptPair ReadOptionalPair(rapidjson::Value const& object, std::string_view const key) {
+//   auto itr = object.FindMember(key.data());
 
-    if (itr->value.Size() >= 2) {
-      y = itr->value[1].GetFloat();
-      return OptPair(std::optional<float>(x), std::optional<float>(y));
-    }
-    return OptPair(std::optional<float>(x), std::nullopt);
-  }
-  return OptPair(std::nullopt, std::nullopt);
-}
+//   if (itr != object.MemberEnd() && itr->value.Size() >= 1) {
+//     float x = itr->value[0].GetFloat();
+//     float y = 0;
 
-static std::optional<NEVector::Quaternion> ReadOptionalRotation(rapidjson::Value const& object,
-                                                                std::string_view const key) {
-  auto itr = object.FindMember(key.data());
-  if (itr != object.MemberEnd()) {
-    NEVector::Vector3 rot;
-    if (itr->value.IsArray() && itr->value.Size() >= 3) {
-      float x = itr->value[0].GetFloat();
-      float y = itr->value[1].GetFloat();
-      float z = itr->value[2].GetFloat();
-      rot = NEVector::Vector3(x, y, z);
-    } else if (itr->value.IsNumber()) {
-      rot = NEVector::Vector3(0, itr->value.GetFloat(), 0);
-    }
+//     if (itr->value.Size() >= 2) {
+//       y = itr->value[1].GetFloat();
+//       return OptPair(std::optional<float>(x), std::optional<float>(y));
+//     }
+//     return OptPair(std::optional<float>(x), std::nullopt);
+//   }
+//   return OptPair(std::nullopt, std::nullopt);
+// }
 
-    return NEVector::Quaternion::Euler(rot);
-  }
-  return std::nullopt;
-}
+// static std::optional<NEVector::Quaternion> ReadOptionalRotation(rapidjson::Value const& object,
+//                                                                 std::string_view const key) {
+//   auto itr = object.FindMember(key.data());
+//   if (itr != object.MemberEnd()) {
+//     NEVector::Vector3 rot;
+//     if (itr->value.IsArray() && itr->value.Size() >= 3) {
+//       float x = itr->value[0].GetFloat();
+//       float y = itr->value[1].GetFloat();
+//       float z = itr->value[2].GetFloat();
+//       rot = NEVector::Vector3(x, y, z);
+//     } else if (itr->value.IsNumber()) {
+//       rot = NEVector::Vector3(0, itr->value.GetFloat(), 0);
+//     }
 
-static std::optional<NEVector::Vector3> ReadOptionalVector3(rapidjson::Value const& object,
-                                                            std::string_view const key) {
-  auto itr = object.FindMember(key.data());
-  if (itr != object.MemberEnd() && itr->value.Size() >= 3) {
-    float x = itr->value[0].GetFloat();
-    float y = itr->value[1].GetFloat();
-    float z = itr->value[2].GetFloat();
-    return NEVector::Vector3(x, y, z);
-  }
-  return std::nullopt;
-}
+//     return NEVector::Quaternion::Euler(rot);
+//   }
+//   return std::nullopt;
+// }
 
-static std::optional<std::array<std::optional<float>, 3>> ReadOptionalScale(rapidjson::Value const& object,
-                                                                            std::string_view const key) {
-  auto itr = object.FindMember(key.data());
-  if (itr != object.MemberEnd() && itr->value.IsArray()) {
-    rapidjson::SizeType size = itr->value.Size();
-    std::optional<float> x = size >= 1 ? std::optional{ itr->value[0].GetFloat() } : std::nullopt;
-    std::optional<float> y = size >= 2 ? std::optional{ itr->value[1].GetFloat() } : std::nullopt;
-    std::optional<float> z = size >= 3 ? std::optional{ itr->value[2].GetFloat() } : std::nullopt;
-    return { { x, y, z } };
-  }
-  return std::nullopt;
-}
+// static std::optional<NEVector::Vector3> ReadOptionalVector3(rapidjson::Value const& object,
+//                                                             std::string_view const key) {
+//   auto itr = object.FindMember(key.data());
+//   if (itr != object.MemberEnd() && itr->value.Size() >= 3) {
+//     float x = itr->value[0].GetFloat();
+//     float y = itr->value[1].GetFloat();
+//     float z = itr->value[2].GetFloat();
+//     return NEVector::Vector3(x, y, z);
+//   }
+//   return std::nullopt;
+// }
 
-} // namespace NEJSON
+// static std::optional<std::array<std::optional<float>, 3>> ReadOptionalScale(rapidjson::Value const& object,
+//                                                                             std::string_view const key) {
+//   auto itr = object.FindMember(key.data());
+//   if (itr != object.MemberEnd() && itr->value.IsArray()) {
+//     rapidjson::SizeType size = itr->value.Size();
+//     std::optional<float> x = size >= 1 ? std::optional{ itr->value[0].GetFloat() } : std::nullopt;
+//     std::optional<float> y = size >= 2 ? std::optional{ itr->value[1].GetFloat() } : std::nullopt;
+//     std::optional<float> z = size >= 3 ? std::optional{ itr->value[2].GetFloat() } : std::nullopt;
+//     return { { x, y, z } };
+//   }
+//   return std::nullopt;
+// }
+
+// } // namespace NEJSON
