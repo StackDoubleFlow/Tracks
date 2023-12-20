586fd31732d0c4b583b8e22f664ecd1c1199c697
FernTheDev <15272073+Fernthedev@users.noreply.github.com>
2023-12-20 00:05:40 -0400

    Nicer header

--- shared/Animation/GameObjectTrackController.hpp
+++ shared/Animation/GameObjectTrackController.hpp
@@ -20,7 +20,6 @@ struct GameObjectTrackControllerData {
   GameObjectTrackControllerData(std::vector<Track*> track, bool v2) : _track(std::move(track)), v2(v2) {}
 
   std::vector<Track*> const _track;
-
   bool const v2;
 
   UnorderedEventCallback<> PositionUpdate;
@@ -31,26 +30,23 @@ struct GameObjectTrackControllerData {
 
 DECLARE_CLASS_CODEGEN(Tracks, GameObjectTrackController, UnityEngine::MonoBehaviour,
                       private:
-                      DECLARE_INSTANCE_FIELD(UnityEngine::Transform*, parent);
-                      DECLARE_INSTANCE_FIELD(UnityEngine::Transform*, origin);
                       static bool LeftHanded;
 
-                      private:
-                      // This is retrived from the data map since Unity doesn't copy it.
+                      DECLARE_INSTANCE_FIELD(UnityEngine::Transform*, parent);
+                      DECLARE_INSTANCE_FIELD(UnityEngine::Transform*, origin);
                       GameObjectTrackControllerData* data;
                       uint64_t lastCheckedTime;
                       int attemptedTries;
 
                       public:
-                      void UpdateData(bool force);
-                      GameObjectTrackControllerData & getTrackControllerData();
-
                       static std::optional<GameObjectTrackController*> HandleTrackData(
-                          UnityEngine::GameObject * gameObject, std::vector<Track*> const& track,
-                          float noteLinesDistance, bool v2, bool overwrite);
+                        UnityEngine::GameObject * gameObject, std::vector<Track*> const& track,
+                        float noteLinesDistance, bool v2, bool overwrite);
 
                       static void ClearData();
 
+                      void UpdateData(bool force);
+                      GameObjectTrackControllerData  getTrackControllerData();
                       DECLARE_INSTANCE_METHOD(void, Awake);
                       DECLARE_INSTANCE_METHOD(void, OnDestroy);
                       DECLARE_INSTANCE_METHOD(void, OnEnable);
@@ -59,4 +55,4 @@ DECLARE_CLASS_CODEGEN(Tracks, GameObjectTrackController, UnityEngine::MonoBehavi
 
                       DECLARE_SIMPLE_DTOR();
                       DECLARE_DEFAULT_CTOR();
-                      )
+)
