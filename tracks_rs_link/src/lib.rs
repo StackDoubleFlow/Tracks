e9148c59b321bc39c2569bbc300a9a714af82ebb
FernTheDev <15272073+Fernthedev@users.noreply.github.com>
2025-03-30 14:28:26 -0400

    Add Android logger integration and panic handling. Update dependencies

--- tracks_rs_link/src/lib.rs
+++ tracks_rs_link/src/lib.rs
@@ -1,1 +1,39 @@
 pub extern crate tracks_rs;
+
+use log::{error, info, LevelFilter};
+use std::panic::PanicHookInfo;
+use android_logger::Config;
+use std::backtrace::Backtrace;
+
+#[ctor::ctor]
+fn main() {
+    android_logger::init_once(Config::default().with_max_level(LevelFilter::Trace));
+
+    std::panic::set_hook(panic_hook(true, true));
+}
+
+/// Returns a panic handler, optionally with backtrace and spantrace capture.
+pub fn panic_hook(
+    backtrace: bool,
+    spantrace: bool,
+) -> Box<dyn Fn(&PanicHookInfo) + Send + Sync + 'static> {
+    // Mostly taken from https://doc.rust-lang.org/src/std/panicking.rs.html
+    Box::new(move |info| {
+        let location = info.location().unwrap();
+        let msg = match info.payload().downcast_ref::<&'static str>() {
+            Some(s) => *s,
+            None => match info.payload().downcast_ref::<String>() {
+                Some(s) => &s[..],
+                None => "Box<dyn Any>",
+            },
+        };
+
+        info!(target: "panic", "panicked at '{}', {}", msg, location);
+        if backtrace {
+            error!(target: "panic", "{:?}", Backtrace::force_capture());
+        }
+        if spantrace {
+            // error!(target: "panic", "{:?}", SpanTrace::capture());
+        }
+    })
+}
